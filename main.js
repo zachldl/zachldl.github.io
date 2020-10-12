7cac24408658356d617de10e852f7f6c991585e7
Zachary Davey Lambert <zach@zldl.co.uk>
2020-10-12 19:20:22 +0100

    Minor cosmetic changes

--- main.js
+++ main.js
@@ -7,15 +7,16 @@ $(document).ready(function(){
 	setTimeout(typeWriter, wait);
 });
 
-function typeWriter(){
+const typeWriter = () => {
 	if(i < content.length){
 		$("#bottomtext").append(content.charAt(i));
 		i++;
 		setTimeout(typeWriter, speed);
-	} else {
-		setTimeout(function(){
-			$("#bottomtext").css("animation", "none");
-			$("#bottomtext").css("border", "none");
-		 }, 2700);
-	};
+	}
+	// else {
+		// setTimeout(function(){
+			// $("#bottomtext").css("animation", "none");
+			// $("#bottomtext").css("border", "none");
+		//  }, 2700);
+	// };
 }
